139dbf7371e825e52570160529e5331312f7c07c
cosmicd <sans11192@gmail.com>
2018-03-28 16:48:06 +0000

    skyed build update.

--- demos/web/skyed/static/js/gmap.js
+++ demos/web/skyed/static/js/gmap.js
@@ -1,1 +1,27 @@
-(function(o,e,n){function t(o,e){var n=new google.maps.Map(document.getElementById(e),{zoom:8,center:o,zoomControl:!0,scaleControl:!0,fullscreenControl:!0});new google.maps.Marker({position:o,map:n})}o.geocoder=new google.maps.Geocoder,o.mapAddressToLatLng=function(e,n,a){o.geocoder.geocode({address:e},function(o,e){if(e==google.maps.GeocoderStatus.OK){var g=o[0].geometry.location,l={lat:g.lat(),lng:g.lng()};a(l),t(l,n)}else window.alert("Request failed.")})},o.mapLatLngToAddress=function(e,n,a){o.geocoder.geocode({location:e},function(o,n){a(e),t(e)})}})(window.gmap=window.gmap||{},jQuery);
+(function(o, e, n) {
+  function t(o, e) {
+    var n = new google.maps.Map(document.getElementById(e), {
+      zoom: 8,
+      center: o,
+      zoomControl: !0,
+      scaleControl: !0,
+      fullscreenControl: !0
+    });
+    new google.maps.Marker({ position: o, map: n });
+  }
+  (o.geocoder = new google.maps.Geocoder()),
+    (o.mapAddressToLatLng = function(e, n, a) {
+      o.geocoder.geocode({ address: e }, function(o, e) {
+        if (e == google.maps.GeocoderStatus.OK) {
+          var g = o[0].geometry.location,
+            l = { lat: g.lat(), lng: g.lng() };
+          a(l), t(l, n);
+        } else window.alert("Request failed.");
+      });
+    }),
+    (o.mapLatLngToAddress = function(e, n, a) {
+      o.geocoder.geocode({ location: e }, function(o, n) {
+        a(e), t(e);
+      });
+    });
+})((window.gmap = window.gmap || {}), jQuery);
--- demos/web/skyed/static/js/misc.js
+++ demos/web/skyed/static/js/misc.js
@@ -1,1 +1,11 @@
-(function(n,s,d){n.show=function(i){i.hasClass("hidden")&&i.removeClass("hidden")},n.hide=function(i){i.hasClass("hidden")||i.addClass("hidden")},n.clearforms=function(n){for(i=0;i<n.length;i++)document.forms[n[i]].reset()}})(window.misc=window.misc||{},jQuery);
+(function(n, s, d) {
+  (n.show = function(i) {
+    i.hasClass("hidden") && i.removeClass("hidden");
+  }),
+    (n.hide = function(i) {
+      i.hasClass("hidden") || i.addClass("hidden");
+    }),
+    (n.clearforms = function(n) {
+      for (i = 0; i < n.length; i++) document.forms[n[i]].reset();
+    });
+})((window.misc = window.misc || {}), jQuery);
